f700f97c89dfbf1328ab4322457dc9a957903bc6
Ronil Gwalani <ronilgwalani@colourmoon.com>
2023-12-06 13:14:30 +0530

    RegistrationSetupComplete

--- app/src/main/java/com/ron/sinch/MainActivity.kt
+++ app/src/main/java/com/ron/sinch/MainActivity.kt
@@ -1,21 +1,21 @@
 package com.ron.sinch
 
 import android.app.Activity
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.messaging.FirebaseMessaging
 import com.ron.sinch.databinding.ActivityMainBinding
 import com.ron.sinchcalling.RonSinch
+import com.ron.sinchcalling.callbacks.PushTokenUnregisterCallback
 import com.ron.sinchcalling.models.RonSinchCallResult
-import com.ron.sinchcalling.models.RonSinchUserModel
 import com.ron.sinchcalling.models.UserCallModel
-import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     private var sp: SharedPreferences? = null
@@ -24,87 +24,78 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        sp = getSharedPreferences("offline", MODE_PRIVATE)
-        val yourUserID = findViewById<TextView>(R.id.your_user_id)
-        val userID: String? = getUserId()
-        initListners()
-        FirebaseMessaging.getInstance().token.addOnSuccessListener {
-            RonSinch(this).registerUser(
-                RonSinchUserModel(
-                    secret = "yKEcNvop6UaLec7WIJii5g==",
-                    userID = userID ?: "",
-                    fcmSenderID = "873551401245",
-                    environment = "ocra.api.sinch.com",
-                    fcmToken = it,
-                    key = "0feeb639-ba6b-45aa-af22-a081e8967bda",
-                    userName = "Ronil-$userID" ?: ""
-                )
-            )
-        }
+        sp = getSharedPreferences(this.packageName, MODE_PRIVATE)
+        val userID: String? = sp!!.getString("userId", "")
+        initListeners()
 
-//        RonSinch(this).signOut(object :PushTokenUnregisterCallback{
-//            override fun onPushTokenUnregistered() {
-//                Log.e("onPushTokenUnregistered", ": ", )
-//            }
-//
-//            override fun onPushTokenUnRegistrationFailed(error: String?) {
-//                Log.e("onPushTokenUnregistered", ":ERROR ->  $error", )
-//            }
-//        })
-        yourUserID.text = "Your User ID :$userID"
+        binding.yourUserId.text = "Your User ID :$userID"
 
     }
 
-    private fun initListners() {
+    private fun initListeners() {
         binding.btnVoiceCall.setOnClickListener {
             RonSinch(this).placeVoiceCall(
                 UserCallModel(
                     binding.etTargetID.text.toString(),
-                    receiverName = "Temp name ${binding.etTargetID.text.toString()}",
+                    receiverName = binding.etUserName.text.toString(),
                 ),
                 launcher = launcher,
                 seconds = 11
             )
         }
         binding.btnVideoCall.setOnClickListener {
+            if (binding.etTargetID.text.toString().length < 5) {
+                Toast.makeText(
+                    this,
+                    "UserID must be valid(length greater then 5)",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+            if (binding.etUserName.text.toString().length < 5) {
+                Toast.makeText(
+                    this,
+                    "etUserName must be valid(length greater then 5)",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
             RonSinch(this).placeVideoCall(
                 UserCallModel(
                     binding.etTargetID.text.toString(),
-                    receiverName = "chota Don"
+                    receiverName = binding.etUserName.text.toString()
                 ), seconds = 111
             )
         }
+
+        binding.logout.setOnClickListener {
+            AlertDialog.Builder(this).setTitle("Alert").setMessage("Are u Sure u wanna Logout")
+                .setPositiveButton("Yes") { _, _ ->
+                    logoutFromSinch()
+                }.setNegativeButton("Cancel") { _, _ ->
+                }.show()
+        }
     }
 
-    private fun generateUserID(): String? {
-        val builder = StringBuilder()
-        val random = Random()
-        while (builder.length < 5) {
-            val nextInt = random.nextInt(10)
-            if (builder.isEmpty() && nextInt == 0) {
-                continue
+    private fun logoutFromSinch() {
+        RonSinch(this).signOut(object : PushTokenUnregisterCallback {
+            override fun onPushTokenUnregistered() {
+                Toast.makeText(
+                    this@MainActivity,
+                    "User Logged out Successfully",
+                    Toast.LENGTH_SHORT
+                ).show()
+                sp?.edit()?.clear()?.apply()
+                startActivity(Intent(this@MainActivity, SplashActivity::class.java))
+                finishAffinity()
             }
-            builder.append(nextInt)
-        }
-        saveUserId(builder.toString())
-        return builder.toString()
-    }
 
-    private fun saveUserId(userId: String) {
-        val edit = sp!!.edit()
-        edit.putString("userId", userId)
-        edit.apply()
+            override fun onPushTokenUnRegistrationFailed(error: String?) {
+                Toast.makeText(this@MainActivity, error, Toast.LENGTH_SHORT).show()
+                Log.e("onPushTokenUnregistered", ":ERROR ->  $error")
+            }
+        })
     }
-
-    private fun getUserId(): String? {
-        val spValue = sp!!.getString("userId", "")
-        return if (spValue!!.isEmpty()) {
-            generateUserID()
-        } else {
-            spValue
-        }
-    }
-
 
     private val launcher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
---
+++ app/src/main/java/com/ron/sinch/RegisterActivity.kt
@@ -0,0 +1,87 @@
+package com.ron.sinch
+
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.messaging.FirebaseMessaging
+import com.ron.sinch.databinding.ActivityRegisterBinding
+import com.ron.sinch.databinding.ProgressDialogueBinding
+import com.ron.sinchcalling.RonSinch
+import com.ron.sinchcalling.callbacks.UserRegisterCallbacks
+import com.ron.sinchcalling.models.RonSinchUserModel
+
+class RegisterActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityRegisterBinding.inflate(layoutInflater) }
+    private val progressBar by lazy {
+        AlertDialog.Builder(this).setView(ProgressDialogueBinding.inflate(layoutInflater).root)
+            .create().also {
+                it.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.register.setOnClickListener {
+            if (binding.etTargetID.text.toString().length < 5) {
+                Toast.makeText(
+                    this,
+                    "UserID must be valid(length greater then 5)",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+            if (binding.etUserName.text.toString().length < 5) {
+                Toast.makeText(
+                    this,
+                    "etUserName must be valid(length greater then 5)",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+            registerUser(binding.etTargetID.text.toString(), binding.etUserName.text.toString())
+
+        }
+    }
+
+    private fun registerUser(userId: String, userName: String) {
+        progressBar.show()
+        FirebaseMessaging.getInstance().token.addOnSuccessListener {
+            RonSinch(this).registerUser(
+                RonSinchUserModel(
+                    secret = "yKEcNvop6UaLec7WIJii5g==",
+                    userID = userId,
+                    fcmSenderID = "873551401245",
+                    environment = "ocra.api.sinch.com",
+                    fcmToken = it,
+                    key = "0feeb639-ba6b-45aa-af22-a081e8967bda",
+                    userName = userName
+                ), object : UserRegisterCallbacks {
+                    override fun onUserRegistered() {
+                        val sp =
+                          getSharedPreferences(this@RegisterActivity.packageName, MODE_PRIVATE)
+                        sp.edit().putString("userId", userId).apply()
+                        Toast.makeText(
+                            this@RegisterActivity,
+                            "User Registration Successful",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        progressBar.dismiss()
+                        startActivity(Intent(this@RegisterActivity, MainActivity::class.java))
+                        finishAffinity()
+                    }
+
+                    override fun onUserRegistrationFailed(error: String?) {
+                        Toast.makeText(this@RegisterActivity, "$error", Toast.LENGTH_SHORT).show()
+                        progressBar.dismiss()
+                    }
+                }
+            )
+        }
+
+    }
+}
